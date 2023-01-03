6dc5a55f6e335da99b85b7aec72faa22e0b235b7
Byron Keet <keetbis@gmail.com>
2023-01-03 11:39:59 +0200

    Added dummy coffee store data to application and loop through it

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import Banner from '../components/Banner';
 import Card from '../components/card';
 
+import coffeeStores from '../data/coffee-stores.json';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -26,7 +28,18 @@ export default function Home() {
 			<div className={styles.heroImage}>
 				<Image src='/static/hero-image.png' width={700} height={400} alt='banner image'/>
 			</div>
-			<Card name='DarkHorse Coffee' imgUrl='/static/hero-image.png' href='coffee-store/darkhorse-coffee' />
+			<div className={styles.cardLayout}>
+				{coffeeStores.map((coffeeStore) => (
+					<Card
+						key={coffeeStore.id}
+						name={coffeeStore.name}
+						imgUrl={coffeeStore.imgUrl}
+						href={`coffee-store/${coffeeStore.id}`}
+						className={styles.card}
+					/>
+				))}
+
+			</div>
 		</main>
 		</div>
 	)
