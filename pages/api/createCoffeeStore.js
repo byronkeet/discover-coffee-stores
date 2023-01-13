33834e0563f2f4e5d4a368974e9c353e979fcda1
Byron Keet <keetbis@gmail.com>
2023-01-13 14:00:16 +0200

    Fixed conditional

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -3,48 +3,52 @@ import { table, getMinifiedRecords, findRecordByFilter } from "../../lib/airtabl
 
 const createCoffeeStore = async (req, res) => {
 
-	if (req.method === 'POST') {
-		const { id, name, address, neighborhood, voting, imgUrl } = req.body;
-		try {
-			if (!id) {
+	if (req.method !== 'POST') {
+		res.status(500);
+		res.json({ message: 'Must be POST request' });
+		return;
+	}
+
+	const { id, name, address, neighborhood, voting, imgUrl } = req.body;
+	try {
+		if (!id) {
+			res.status(400);
+			res.json({ message: 'Id is missing' })
+			return;
+		}
+
+		const records = await findRecordByFilter(id);
+
+		if (records.length !== 0) {
+			res.json(records);
+		} else {
+			// create a record
+			if (!name) {
 				res.status(400);
-				res.json({ message: 'Id is missing' })
+				res.json({ message: 'Name is missing' });
 				return;
 			}
 
-			const records = await findRecordByFilter(id);
-
-			if (records.length !== 0) {
-				res.json(records);
-			} else {
-				// create a record
-				if (!name) {
-					res.status(400);
-					res.json({ message: 'Name is missing' });
-					return;
-				}
-
-				const createRecords = await table.create([
-					{
-						fields: {
-							id,
-							name,
-							address,
-							neighborhood,
-							voting,
-							imgUrl
-						}
+			const createRecords = await table.create([
+				{
+					fields: {
+						id,
+						name,
+						address,
+						neighborhood,
+						voting,
+						imgUrl
 					}
-				]);
-				const records = getMinifiedRecords(createRecords);
-				res.json(records)
-			}
-		} catch (err) {
-			console.error('Error creating or finding store', err);
-			res.status(500);
-			res.json({ message: 'Error creating or finding store', err })
+				}
+			]);
+			const records = getMinifiedRecords(createRecords);
+			res.json(records)
 		}
-	};
+	} catch (err) {
+		console.error('Error creating or finding store', err);
+		res.status(500);
+		res.json({ message: 'Error creating or finding store', err })
+	}
 }
 
 export default createCoffeeStore;
